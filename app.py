9a34273897bf7a1f3f41c16a387c0e63ee32e062
juana nakfour <jnakfour@redhat.com>
2019-02-20 10:35:30 -0600

    removed reading file

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ from pyspark.sql import SparkSession
 print("Start")
 #on openshift
 spark = SparkSession.builder.appName("odh-pyspark").getOrCreate()
-df=spark.read.csv('sample.csv',header=True)
-print(df.head())
+#df=spark.read.csv('sample.csv',header=True)
+#print(df.head())
 
