df8574667c19ac84d7e68630ef9fa61c0aa91e5c
juana nakfour <jnakfour@redhat.com>
2019-02-20 10:05:06 -0600

    Added a sample csv file and corrected the spark session builder command

--- app.py
+++ app.py
@@ -4,13 +4,7 @@ import pyspark
 
 print("Start")
 #on openshift
-#spark = SparkSession.builder.appName("mobileanalytics").config("spark.mongodb.input.uri", "mongodb://admin:admin@mongodb/sampledb.bikerental").config("spark.mongodb.output.uri", "mongodb://admin:admin@mongodb/sampledb.bikerental").getOrCreate()
-#Set the configuration
-conf = pyspark.SparkConf().setAppName('ODH PySpark').setMaster('spark://' + os.environ['OSHINKO_CLUSTER_NAME'] + ':7077')
+spark = SparkSession.builder.appName("odh-pyspark").getOrCreate()
+df=spark.read.csv('sample.csv',header=True)
+print(df.head())
 
-#Set the Spark cluster connection
-sc = pyspark.SparkContext.getOrCreate(conf)
-
-
-#Get the SQL context
-sqlContext = pyspark.SQLContext(sc)
